701b7368d8457b77dc1d1568416a0f76c999bdb0
Violetcv <you@example.com>
2024-07-14 19:49:46 +0530

    feat: clients

--- src/components/clients.tsx
+++ src/components/clients.tsx
@@ -34,9 +34,9 @@ export default function Clients() {
       </p>
       <div className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-6'>
         {clients.map((client) => (
-          <div key={client.name} className='flip-card'>
-            <div className='flip-card-inner'>
-              <div className='flip-card-front'>
+          <div key={client.name} className='group h-100 w-100 [perspective:1000]'>
+            <div className='text-center transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]'>
+              <div className='relative inset-0 flex items-center justify-center [backface-visibility:hidden]'>
                 <Image
                   src={client.logo}
                   alt={client.name}
@@ -45,57 +45,13 @@ export default function Clients() {
                   className='h-16 w-auto'
                 />
               </div>
-              <div className='flip-card-back'>
-                <div className='tooltip'>{client.name}</div>
+              <div className='absolute inset-0 flex items-center justify-center [transform:rotateY(180deg)] [backface-visibility:hidden]'>
+                <div className='bg-white/40 text-black '>{client.name}</div>
               </div>
             </div>
           </div>
         ))}
       </div>
-      <style jsx>{`
-        .flip-card {
-          background-color: transparent;
-          perspective: 1000px;
-        }
-        .flip-card-inner {
-          position: relative;
-          width: 100%;
-          height: 100%;
-          text-align: center;
-          transition: transform 0.6s;
-          transform-style: preserve-3d;
-        }
-        .flip-card:hover .flip-card-inner {
-          transform: rotateY(180deg);
-        }
-        .flip-card-front,
-        .flip-card-back {
-          position: relative;
-          width: 100%;
-          backface-visibility: hidden;
-        }
-        .flip-card-front {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        .flip-card-back {
-          // background-color: #333;
-          margin-top: -70px;
-          color: white;
-          transform: rotateY(180deg);
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        .tooltip {
-          padding: 10px;
-          border-radius: 6px;
-          color: #000000;
-          text-align: center;
-          width: 120px;
-        }
-      `}</style>
     </div>
   );
 }
