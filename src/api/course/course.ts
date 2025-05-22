2acdf2f3cacb654c57b2e66d53487cd7f83fa836
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2025-05-22 10:23:12 -0700

    date update

--- src/api/course/course.ts
+++ src/api/course/course.ts
@@ -377,7 +377,7 @@ const detailedCourses: CourseDetails[] = [
         description: 'Multiple Case Studies; Hands-on Practice.',
       },
     ],
-    startDate: '2025-05-17',
+    startDate: '2025-05-27',
     whatYouWillLearn: [
       'Descriptive Statistics - It includes methodologies such as cross-tabulation, frequencies, and descriptive ratio statistics.',
       'Bivariate Statistics - It includes methodologies such as means, nonparametric tests, correlation, and Analysis of Variance (ANOVA).',
